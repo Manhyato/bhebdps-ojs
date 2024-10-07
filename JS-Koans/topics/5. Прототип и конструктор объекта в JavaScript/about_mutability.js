0e97e142c2a013fb6a545043824afcd766309858
Manhyato <145689933+Manhyato@users.noreply.github.com>
2024-10-07 13:30:05 +0300

    Дз5

--- JS-Koans/topics/5. Прототип и конструктор объекта в JavaScript/about_mutability.js
+++ JS-Koans/topics/5. Прототип и конструктор объекта в JavaScript/about_mutability.js
@@ -1,9 +1,9 @@
 describe("About Mutability (about_mutability.js)", function() {
   it("should expect object properties to be public and mutable", function () {
-    let aPerson = { firstname: "John", lastname: "Smith" };
-    aPerson.firstname = "Alan";
+    let aPerson = { firstname: 'John', lastname: 'Smith' };
+    aPerson.firstname = 'Alan';
 
-    expect(aPerson.firstname).toBe(FILL_ME_IN);
+    expect(aPerson.firstname).toBe('Alan');
   });
 
   it("should understand that constructed properties are public and mutable", function () {
@@ -11,10 +11,10 @@ describe("About Mutability (about_mutability.js)", function() {
       this.firstname = firstname;
       this.lastname = lastname;
     }
-    let aPerson = new Person("John", "Smith");
-    aPerson.firstname = "Alan";
+    let aPerson = new Person('John', 'Smith');
+    aPerson.firstname = 'Alan';
 
-    expect(aPerson.firstname).toBe(FILL_ME_IN);
+    expect(aPerson.firstname).toBe('Alan');
   });
   
   it("should expect prototype properties to be public and mutable", function () {
@@ -27,13 +27,13 @@ describe("About Mutability (about_mutability.js)", function() {
     };
   
     let aPerson = new Person("John", "Smith");
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    expect(aPerson.getFullName()).toBe('John Smith');
   
     aPerson.getFullName = function () {
       return this.lastname + ", " + this.firstname;
     };
   
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    expect(aPerson.getFullName()).toBe('Smith, John');
   });
   
   it("should know that variables inside a constructor and constructor args are private", function () {
@@ -44,20 +44,20 @@ describe("About Mutability (about_mutability.js)", function() {
       this.getLastName = function () { return lastname; };
       this.getFullName = function () { return fullName; };
     }
-    let aPerson = new Person("John", "Smith");
+    let aPerson = new Person('John', 'Smith');
 
-    aPerson.firstname = "Penny";
-    aPerson.lastname = "Andrews";
-    aPerson.fullName = "Penny Andrews";
+    aPerson.firstname = 'Penny';
+    aPerson.lastname = 'Andrews';
+    aPerson.fullName = 'Penny Andrews';
   
-    expect(aPerson.getFirstName()).toBe(FILL_ME_IN);
-    expect(aPerson.getLastName()).toBe(FILL_ME_IN);
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    expect(aPerson.getFirstName()).toBe('John');
+    expect(aPerson.getLastName()).toBe('Smith');
+    expect(aPerson.getFullName()).toBe('John Smith');
 
     aPerson.getFullName = function () {
       return aPerson.lastname + ", " + aPerson.firstname;
     };
 
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    expect(aPerson.getFullName()).toBe('Andrews, Penny');
   });
 });
--- JS-Koans/topics/5. Прототип и конструктор объекта в JavaScript/about_objects.js
+++ JS-Koans/topics/5. Прототип и конструктор объекта в JavaScript/about_objects.js
@@ -4,56 +4,56 @@ describe("About Objects (about_objects.js)", function () {
     let megalomaniac;
 
     beforeEach(function () {
-       megalomaniac = {  mastermind: "Joker", henchwoman: "Harley" };
+       megalomaniac = {  mastermind: 'Joker', henchwoman: 'Harley' };
     });
 
     it("should confirm objects are collections of properties", function () {
-      expect(megalomaniac.mastermind).toBe(FILL_ME_IN);
+        expect(megalomaniac.mastermind).toBe('Joker');
     });
 
     it("should confirm that properties are case sensitive", function () {
-      expect(megalomaniac.henchwoman).toBe(FILL_ME_IN);
-      expect(megalomaniac.henchWoman).toBe(FILL_ME_IN);
+      expect(megalomaniac.henchwoman).toBe('Harley');
+      expect(megalomaniac.henchWoman).toBe(undefined);
     });
   });
 
 
   it("should know properties that are functions act like methods", function () {
     let megalomaniac = {
-      mastermind : "Brain",
-      henchman: "Pinky",
+      mastermind : 'Brain',
+      henchman: 'Pinky',
       battleCry: function (noOfBrains) {
-        return "They are " + this.henchman + " and the" +
-          Array(noOfBrains + 1).join(" " + this.mastermind);
+        return 'They are ' + this.henchman + ' and the' +
+          Array(noOfBrains + 1).join(' ' + this.mastermind);
       }
     };
 
     let battleCry = megalomaniac.battleCry(4);
-    expect(FILL_ME_IN).toMatch(battleCry);
+      expect('They are Pinky and the Brain Brain Brain Brain Brain').toMatch(battleCry);
   });
 
   it("should confirm that when a function is attached to an object, 'this' refers to the object", function () {
     let currentDate = new Date();
     let currentYear = (currentDate.getFullYear());
     let megalomaniac = {
-      mastermind: "James Wood",
-      henchman: "Adam West",
+      mastermind: 'James Wood',
+      henchman: 'Adam West',
       birthYear: 1970,
       calculateAge: function () {
         return currentYear - this.birthYear;
       }
     };
 
-    expect(currentYear).toBe(FILL_ME_IN);
-    expect(megalomaniac.calculateAge()).toBe(FILL_ME_IN);
+    expect(currentYear).toBe(2024);
+    expect(megalomaniac.calculateAge()).toBe(54);
   });
 
   describe("'in' keyword", function () {
     let megalomaniac;
     beforeEach(function () {
       megalomaniac = {
-        mastermind: "The Monarch",
-        henchwoman: "Dr Girlfriend",
+        mastermind: 'The Monarch',
+        henchwoman: 'Dr Girlfriend',
         theBomb: true
       };
     });
@@ -61,26 +61,26 @@ describe("About Objects (about_objects.js)", function () {
     it("should have the bomb", function () {
       let hasBomb = "theBomb" in megalomaniac;
 
-      expect(hasBomb).toBe(FILL_ME_IN);
+      expect(hasBomb).toBe(true);
     });
 
     it("should not have the detonator however", function () {
       let hasDetonator = "theDetonator" in megalomaniac;
 
-      expect(hasDetonator).toBe(FILL_ME_IN);
+      expect(hasDetonator).toBe(false);
     });
   });
 
   it("should know that properties can be added and deleted", function () {
     let megalomaniac = { mastermind : "Agent Smith", henchman: "Agent Smith" };
 
-    expect("secretary" in megalomaniac).toBe(FILL_ME_IN);
+    expect("secretary" in megalomaniac).toBe(false);
 
     megalomaniac.secretary = "Agent Smith";
-    expect("secretary" in megalomaniac).toBe(FILL_ME_IN);
+    expect("secretary" in megalomaniac).toBe(true);
 
     delete megalomaniac.henchman;
-    expect("henchman" in megalomaniac).toBe(FILL_ME_IN);
+    expect("henchman" in megalomaniac).toBe(false);
   });
 
 
@@ -91,16 +91,16 @@ describe("About Objects (about_objects.js)", function () {
 
       let simpleCircle = new Circle(10);
       let colouredCircle = new Circle(5);
-      colouredCircle.colour = "red";
+      colouredCircle.colour = 'red';
 
-      expect(simpleCircle.colour).toBe(FILL_ME_IN);
-      expect(colouredCircle.colour).toBe(FILL_ME_IN);
+      expect(simpleCircle.colour).toBe(undefined);
+      expect(colouredCircle.colour).toBe('red');
 
       Circle.prototype.describe = function () {
-        return "This circle has a radius of: " + this.radius;
+        return 'This circle has a radius of: ' + this.radius;
       };
 
-      expect(simpleCircle.describe()).toBe(FILL_ME_IN);
-      expect(colouredCircle.describe()).toBe(FILL_ME_IN);
+      expect(simpleCircle.describe()).toBe('This circle has a radius of: 10');
+      expect(colouredCircle.describe()).toBe('This circle has a radius of: 5');
   });
 });
